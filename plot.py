230d762f6a59fb3a71c51ccc23868eafb867dcd2
Simon <Gtingwen@outlook.com>
2019-04-10 17:25:17 -0400

    Plots added

--- plot.py
+++ plot.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 import test
 
 p, n = test.import_experiment_data('data.tsv')
+'''''
 connective = p
 for idx, num in enumerate(p):
     connective[idx] = (p[idx] * (4 ** (idx + 1))) ** (1/(idx + 1))
-print(connective)
+'''''
 # Initialize the plot
 fig = plt.figure(figsize=(10,10))
 ax1 = fig.add_subplot(111)
-ax1.set_title('connective constant against n')
+ax1.set_title('p_bar against n')
 # ax2.set_title('connective constant against n')
 # Plot the data
-ax1.scatter(n, connective)
+ax1.scatter(n, p)
 # ax2.scatter(connective, n)
 
 plt.tight_layout()
 # Show the plot
-plt.savefig('connective_n_fig.png')
+plt.savefig('p_n_fig.png')
